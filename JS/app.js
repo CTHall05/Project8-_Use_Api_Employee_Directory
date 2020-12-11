0a65a9106ec4f5962fb4df3215718a9157d72ca5
Taylor <63585450+Kordorgreyhoof@users.noreply.github.com>
2020-12-11 23:18:09 +0000

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -87,12 +87,12 @@ modalClose.addEventListener('click', () => {
 
 // Search Bar Employee Filter //
 
-const searchBar = document.querySelector("searchTerm");
+const searchBar = document.querySelector(".searchTerm");
 let employeeName = document.querySelectorAll(".card h2");
 searchBar.addEventListener("keyup", (e) => {
   const searchTerm = e.target.value.toLowerCase();
   for (let i = 0; i < employeeName.length; i++) {
-    const searchInput = employeeName[i];
+    const searchInput = employeeName[i].textContent;
     if (searchInput.toLowerCase().includes(searchTerm)) {
       employeeName[i].parentNode.style.display = "";
     } else {
